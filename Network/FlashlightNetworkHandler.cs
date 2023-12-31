3c6936746caa03e557cb2e6939ea229caf999fc0
Cmmmmmm <512065439@qq.com>
2023-12-31 09:40:23 +0000

    rework the whole project Rpc done

---
+++ Network/FlashlightNetworkHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace LethalFlashlight.Network;
+
+public class FlashlightNetworkHandler : NetworkBehaviour{
+    public static FlashlightNetworkHandler Instance { get; private set; }
+    public static event Action<NetworkObjectReference, float> IntensityChangedEvent;
+
+    public override void OnNetworkSpawn() {
+        IntensityChangedEvent = null;
+
+        if (NetworkManager.Singleton.IsHost || NetworkManager.Singleton.IsServer) {
+            Instance?.gameObject.GetComponent<NetworkObject>().Despawn();
+        }
+        
+        Instance = this;
+        Debug.Log("NetworkHandler Spawned!");
+        
+        base.OnNetworkSpawn();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void IntensityEventServerRpc(NetworkObjectReference refObject, float newIntensity){
+        Debug.Log("IntensityEventServerRpc Fired!");
+        IntensityEventClientRpc(refObject, newIntensity);
+    }
+    
+    [ClientRpc]
+    public void IntensityEventClientRpc(NetworkObjectReference refObject, float newIntensity){
+        Debug.Log("IntensityEventClientRpc Fired!");
+        Debug.Log("Object Reference: " + refObject.NetworkObjectId);
+        Debug.Log("New Intensity: " + newIntensity);
+        IntensityChangedEvent?.Invoke(refObject, newIntensity);
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Reflection;
 using BepInEx;
 using HarmonyLib;
+using LethalFlashlight.Network;
 using LethalFlashlight.Patches;
 using UnityEngine;
 
@@ -9,10 +11,31 @@ namespace LethalFlashlight
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        private Harmony harmony = new Harmony("LethalFlashlight");
+        public static AssetBundle MainAssetBundle;
+        
+        private Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+        
         private void Awake()
         {
-            this.harmony.PatchAll(typeof(FlashlightItemPatch));
+            var stream  = Assembly.GetExecutingAssembly().GetManifestResourceStream("LethalFlashlight.Resources.flashlightasset");
+            MainAssetBundle = AssetBundle.LoadFromStream(stream);
+            
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types) {
+                var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+                foreach (var method in methods) {
+                    var attributes = method.GetCustomAttributes(typeof(RuntimeInitializeOnLoadMethodAttribute), false);
+                    if (attributes.Length > 0) {
+                        method.Invoke(null, null);
+                    }
+                }
+            }
+
+            harmony.PatchAll(typeof(FlashlightNetworkHandler));
+            harmony.PatchAll(typeof(FlashlightItemPatch));
+            harmony.PatchAll(typeof(NetworkObjectPath));    
+            
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
